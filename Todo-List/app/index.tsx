8501cff431bb0741ce27f6d8bd2fae68f3ad0755
Arvv_institucional <up240589@upa.edu.mx>
2026-03-29 08:48:51 -0600

    aplied formating on the code

--- Todo-List/app/index.tsx
+++ Todo-List/app/index.tsx
@@ -4,35 +4,39 @@ import { useState } from "react";
 import { Text, View, Pressable } from "react-native";
 
 export default function Index() {
-  const [isVisibleView, setIsVisibleView] = useState(false)
+  const [isVisibleView, setIsVisibleView] = useState(false);
 
-  const updateStateView =() =>{
-    setIsVisibleView(!isVisibleView)
-  }
+  const updateStateView = () => {
+    setIsVisibleView(!isVisibleView);
+  };
 
-  const [isVisibleEdit, setIsVisibleEdit] = useState(false)
+  const [isVisibleEdit, setIsVisibleEdit] = useState(false);
 
-  const updateStateEdit =() =>{
-    setIsVisibleEdit(!isVisibleEdit)
-  }
+  const updateStateEdit = () => {
+    setIsVisibleEdit(!isVisibleEdit);
+  };
 
   return (
     /*Esta parte del codigo posiblemente se tenga que mover a donde se haga el componente de las tarjetas*/
     <View>
-      <ViewModal id={2} isVisibleView={isVisibleView} newState={updateStateView}/>
-      <EditModal id={2} isVisibleEdit={isVisibleEdit} newState={updateStateEdit}/>
+      <ViewModal
+        id={2}
+        isVisibleView={isVisibleView}
+        newState={updateStateView}
+      />
+      <EditModal
+        id={2}
+        isVisibleEdit={isVisibleEdit}
+        newState={updateStateEdit}
+      />
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      
+
       <Pressable onPress={updateStateView}>
-        <Text>
-          Boton para ver
-        </Text>
+        <Text>Boton para ver</Text>
       </Pressable>
 
       <Pressable onPress={updateStateEdit}>
-        <Text>
-          Boton para editar
-        </Text>
+        <Text>Boton para editar</Text>
       </Pressable>
     </View>
   );
